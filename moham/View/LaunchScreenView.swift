ecfed50be3d138aa016ef52590fb81f79755d347
ysh8614 <qwaszx8614@gmail.com>
2023-12-31 23:19:29 +0900

    스플래시 스크린 구현

---
+++ moham/View/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  moham
+//
+//  Created by ysh on 12/31/23.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
