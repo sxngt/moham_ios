19abb8ea356fcf4f08791dde5762f006602072b5
ysh8614 <qwaszx8614@gmail.com>
2023-12-31 22:28:29 +0900

    Initial Commit

---
+++ moham/mohamApp.swift
@@ -0,0 +1,17 @@
+//
+//  mohamApp.swift
+//  moham
+//
+//  Created by ysh on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct mohamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
